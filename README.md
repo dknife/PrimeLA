5708c92aa4c594060a521937e36ab4fd51e92c7a
ymkang <ymkang@tu.ac.kr>
2021-06-21 10:09:04 +0900

    Egg directory removal

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# Linear Algebra Visualization
+#PrimeLA
+### Visualization Package for 'Prime Linear Algebra'
+### '으뜸 선형대수'를 위한 가시화 패키지
 
-This is a simple preliminary package for visualization of vectors (2d and 3d) and matrix (22, 33)
+This is a simple preliminary package 
+for visualizing the vectors (2d and 3d) and matrix (2x2, 3x3)
 
-Author: Young-Min Kang
+[GitHub Repository](https://github.com/dknife/PrimeLA)
+
+Author: Young-Min Kang (c) 2021
 
--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="prime_linear_algebra",
-    version="0.0.3",
+    name="PrimeLA",
+    version="0.0.1",
     author="dknife",
     author_email="young.min.kang@gmail.com",
-    description="A small linear algebra package",
+    description="A small linear algebra visualization package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dknife",
+    url="https://github.com/dknife/PrimeLA",
     project_urls={
-        "Bug Tracker": "https://github.com/dknife",
+        "Bug Tracker": "https://github.com/dknife/PrimeLA",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
